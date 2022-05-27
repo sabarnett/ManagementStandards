0a39d909a34e3934868983e4aba17ad7a4d17944
Steven barnett <steve@sabarnett.co.uk>
2022-05-27 15:23:51 +0100

    Initial Commit

---
+++ Parse Questionnaire/Parse_QuestionnaireApp.swift
@@ -0,0 +1,15 @@
+// Project: Parse Questionnaire
+//
+//  
+//
+
+import SwiftUI
+
+@main
+struct Parse_QuestionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
